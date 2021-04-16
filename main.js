68d0a9ac5388dc42a5ca83c034ad185069799790
GhostFury <thuhuongvnhubt@gmail.com>
2021-04-16 11:28:01 +0700

    update fix/v2

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 const form = document.forms["formFood"];
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
+// Declare
 let getList = localStorage.getItem("getList")
   ? JSON.parse(localStorage.getItem("getList"))
   : [];
 let chooseBtn = document.getElementById("btn-add");
+
+let getID = document.getElementById("foodID");
+let getName = document.getElementById("foodName");
+let getCost = document.getElementById("foodCost");
+
+let findNamebtn = document.getElementById("findByName");
+let searchInput = document.getElementById("searchInput");
+
 let currentID = "";
 document.getElementById("foodID").value = 1;
 
+//add & edit item
+
 function updateFood() {
-  let idFood = document.getElementById("foodID").value;
-  let nameFood = document.getElementById("foodName").value;
-  let costFood = document.getElementById("foodCost").value;
-
-  if (nameFood == "") {
-    document.getElementById("id-messages").innerHTML = "";
-
+  let idFood = getID.value;
+  let nameFood = getName.value;
+  let costFood = getCost.value;
+  if (!nameFood) {
     document.getElementById("name-messages").innerHTML =
       "Name is not invalid, please check !";
     nameFood = "";
@@ -33,24 +38,24 @@ function updateFood() {
       });
     } else {
       getList.splice(currentID, 1, {
-        idFood: idFood,
-        nameFood: nameFood,
-        costFood: costFood,
+        idFood,
+        nameFood,
+        costFood,
       });
       currentID = "";
       chooseBtn.innerHTML = "Add";
     }
     localStorage.setItem("getList", JSON.stringify(getList));
-    this.renderList();
+    renderList();
 
     nameFood = document.getElementById("foodName").value = "";
     costFood = document.getElementById("foodCost").value = "";
   }
 }
-
+//render list
 function renderList() {
-  if (getList === 0) {
-    return false;
+  if (!getList.length) {
+    return;
   }
   let listFood = `
     <tr id = >
@@ -64,103 +69,104 @@ function renderList() {
     id++;
     listFood += `
       <tr id = "tr-food">
-          <td id= "td-id">${id}</td>
+          <td id= "td-id">${food.idFood}</td>
           <td id= "td-name"> ${food.nameFood}</td>
           <td id= "td-cost"> ${food.costFood}</td>
           <td id= "td-select"><a href='#' id = "edit" onclick = "editID(${food.idFood})" >Edit</a> 
             | <a href='#' onclick = "deleteFood(${food.idFood})">Delete</a></td>
       </tr>
       `;
-    document.getElementById("foodID").value = ++id;
+    getID.value = ++id;
   });
-
   document.getElementById("list-food").innerHTML = listFood;
 }
+
+// fill data to edit
+
+function editID(id) {
+  function foodFind(food) {
+    return food.idFood == id;
+  }
+  let foodfinded = getList.find(foodFind);
+
+  currentID = getList.indexOf(foodfinded);
+
+  getID.value = foodfinded["idFood"];
+  getName.value = foodfinded["nameFood"];
+  getCost.value = foodfinded["costFood"];
+
+  chooseBtn.innerHTML = "Save";
+}
+
+//delete item by id
 
 const deleteFood = (id) => {
   const indexFind = getList.findIndex((food) => food.idFood == id);
 
   if (indexFind > -1) {
-    let confirmDelete = confirm("Are you sure that delete this Food ???");
-    if (confirmDelete == true) {
+    const confirmDelete = confirm("Are you sure that delete this Food ???");
+    if (confirmDelete) {
       getList.splice(indexFind, 1);
       localStorage.setItem("getList", JSON.stringify(getList));
       renderList();
-    } else {
-      return false;
     }
   }
 };
-
-function editID(id) {
-  const indexFind = getList.findIndex((food) => food.idFood == id);
-  let food = getList[indexFind];
-  currentID = indexFind;
-  document.getElementById("foodID").value = food["idFood"];
-  document.getElementById("foodName").value = food["nameFood"];
-  document.getElementById("foodCost").value = food["costFood"];
-
-  chooseBtn.innerHTML = "Save";
-}
 
 const totalCost = getList.reduce(sumCost, 0);
 
 function sumCost(cost, currentCost) {
   return cost + Number(currentCost.costFood);
 }
-
 document.getElementById("total").innerHTML = totalCost;
 
 //
 //Sort
+
 function sortBySelect() {
   let selectValue = document.getElementById("foodSort").value;
+  //sort by ID
+  if (selectValue === "option1") {
+    getList.sort((item1, item2) => {
+      return item1.idFood - item2.idFood;
+    });
+    renderList();
+  }
   //sort by Name
-  if (selectValue == "option2") {
+  else if (selectValue === "option2") {
     getList.sort((item1, item2) => {
       if (item1.nameFood < item2.nameFood) {
         return -1;
       }
-      if (item1.nameFood > item2.nameFood) {
-        return 1;
-      }
-      return 0;
     });
-    localStorage.setItem("getList", JSON.stringify(getList));
-    this.renderList();
+    renderList();
   }
   // sort by cost
-  else if (selectValue == "option3") {
+  else if (selectValue === "option3") {
     getList.sort((item1, item2) => {
-      if (item1.costFood < item2.costFood) {
-        return -1;
-      }
-      if (item1.costFood > item2.costFood) {
-        return 1;
-      }
-      return 0;
+      return item1.costFood - item2.costFood;
     });
-    localStorage.setItem("getList", JSON.stringify(getList));
-    this.renderList();
+    renderList();
   }
 }
-//
 
 //Find by field
+
 function searchFood() {
-  let findCostbtn = document.getElementById("findByCost").checked;
-  let searchInput = document.getElementById("searchInput").value;
-  if (findCostbtn === true) {
+  let findNamebtn = document.getElementById("findByName").checked;
+  let convertValue = searchInput.value.toLocaleLowerCase();
+  if (!searchInput.value) {
+    window.location.reload();
+  }
+  if (findNamebtn) {
     getList = getList.filter((listFind) => {
-      return listFind.costFood === searchInput;
+      return listFind.nameFood.toLocaleLowerCase().includes(convertValue);
     });
-    console.log(getList);
-    this.renderList();
+    renderList();
   } else {
     getList = getList.filter((listFind) => {
-      return listFind.nameFood === searchInput;
+      return listFind.costFood.toLocaleLowerCase().includes(convertValue);
     });
-    console.log(getList);
-    this.renderList();
+    renderList();
   }
 }
